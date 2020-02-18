3817666035a8b3624f539b9cc7bfd4281f35154b
Pranshika Singla <Pranshika_Singla@epam.com>
2020-02-18 15:57:20 +0530

    initial commit

---
+++ SQLInjection/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="SQLInjection.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-bottom: 2px;
+        }
+    </style>
+</head>
+<body>
+
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Click here" />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CssClass="auto-style1">
+             <Columns>
+        <asp:BoundField DataField="Name" HeaderText="Name" />
+        <asp:BoundField DataField="Age" HeaderText="Age" />
+    </Columns>
+        </asp:GridView>
+    </form>
+    <asp:SqlDataSource ID="Sample" runat="server" ConnectionString="<%$ ConnectionStrings:SampleConnectionString %>" SelectCommand="SELECT * FROM [Sample_Table]"></asp:SqlDataSource>
+</body>
+</html>
---
+++ SQLInjection/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SQLInjection
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SampleConnectionString"].ConnectionString);
+            SqlCommand com = new SqlCommand("Select * from Sample_Table where Name= @Name", conn);
+            com.Parameters.Add(new SqlParameter("@Name",TextBox1.Text));
+           
+            conn.Open();
+            GridView1.DataSource = com.ExecuteReader();
+            GridView1.DataBind();
+            conn.Close();
+        }
+
+        //SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SampleConnectionString"].ConnectionString);
+        //SqlCommand com = new SqlCommand("Select * from Sample_Table where Name= '" + TextBox1.Text + "'", conn);
+        //conn.Open();
+        //    GridView1.DataSource = com.ExecuteReader();
+        //    GridView1.DataBind();
+        //    conn.Close();
+    }
+}
